e4d919e5cef1b42ca6fd4140b315e3586c5c04e5
Johnny Verbeek <iredcraft@gmail.com>
2016-05-23 12:54:04 +0200

    First commit with basic design

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*
+#     #                                                #     #                  #######                      
+#     # #    # # #    # ###### #####   ####  ######    ##   ##  ####  #####        #     ####   ####  #      
+#     # ##   # # #    # #      #    # #      #         # # # # #    # #    #       #    #    # #    # #      
+#     # # #  # # #    # #####  #    #  ####  #####     #  #  # #    # #    #       #    #    # #    # #      
+#     # #  # # # #    # #      #####       # #         #     # #    # #    #       #    #    # #    # #      
+#     # #   ## #  #  #  #      #   #  #    # #         #     # #    # #    #       #    #    # #    # #      
+ #####  #    # #   ##   ###### #    #  ####  ######    #     #  ####  #####        #     ####   ####  ######
+ 
+        Author  : Jonathan 'Johnny' Verbeek a.k.a iUltimateLP
+        Purpose : Main JavaScript code for the Universe Mod Tool
+*/
+
+const electron = require("electron"); //Electron module
+const app = electron.app //App shortcut
+const BrowserWindow = electron.BrowserWindow; //Browser windows
+
+let mainWindow; //Why let? Because otherwise it gets garbage-collected
+
+function initWindow()
+{
+    mainWindow = new BrowserWindow({width: 900, height: 600, autoHideMenuBar: false, backgroundColor: "#263238"}); //#25373D #282830 #60646D #263238
+    mainWindow.loadURL(`file://${__dirname}/../html/index.html`);
+    
+    mainWindow.on("closed", function() {
+        mainWindow = null;    
+    });
+    
+    console.log("$(__dirname)");
+}
+
+app.on("ready", initWindow); //When Electron has done all it's stuff, init the window!
